5e0aafcb02b376890bdc8853e66cec0024cf7321
aayushr03 <77154489+aayushr03@users.noreply.github.com>
2023-05-13 11:29:19 +0530

    Update precentinc.py

--- precentinc.py
+++ precentinc.py
@@ -11,12 +11,14 @@ print("This refers to CO2 emissions in kilo-tonnes unit")
 # print(data)
 
 labels =["1991","1992","1993","1994","1995","1996","1997","1998","1999","2000","2001","2002","2003","2004","2005","2006","2007","2008","2009","2010","2011","2012","2013","2014","2015","2016","2017","2018","2019"]
-df=data.loc[data["Country Name"]=="Bangladesh"]
+df=data.loc[data["Country Name"]=="China"]
 arr=np.array(df)
 # print(arr)
 lst =[]
+#We store the percentage increases in a list.
 for i in range (5,arr.size):
     lst.append(round((arr[0][i]-arr[0][i-1])*100/arr[0][i-1],2))
+#We print the minimum value in the list as it might be negative and corresponding to that we change plt.ylim
 print(min(lst))
 plt.plot(labels,lst)
 plt.ylim(-10, 100)
